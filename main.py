fafa16861bb8a7062a0f2b32aeb7654b508820b6
avi2501 <104846825+avi2501@users.noreply.github.com>
2024-04-08 02:37:39 +0530

    intial

---
+++ main.py
@@ -0,0 +1,12 @@
+import kivy
+import kivymd
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class myapp(MDApp):
+    def build(self):
+        a= MDLabel(text='welcme ji', halign='center')
+        return a
+    
+if __name__ =='__main__':
+    myapp().run()
